e0322da42fc3aaf8285b3bfb12e3563c4ff337b3
Fabian <frakxman@hotmail.com>
2024-04-19 11:53:42 -0500

    Update routing configuration to include a NotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 
 // Components
 import { ListComponent } from './domains/products/pages/list/list.component';
+import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
