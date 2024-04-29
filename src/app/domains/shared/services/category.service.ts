e05e0565f7a93e42a6ff10a3ed2d242c197a355a
Fabian <frakxman@hotmail.com>
2024-04-29 12:09:45 -0500

    Update category service to use environment baseUrl

--- src/app/domains/shared/services/category.service.ts
+++ src/app/domains/shared/services/category.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
+import { environment } from '@env/environments';
+
 import { Category } from '@shared/models/category.model';
 
 @Injectable({
@@ -8,11 +10,13 @@ import { Category } from '@shared/models/category.model';
 })
 export class CategoryService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+
   private http = inject(HttpClient);
 
   constructor() { }
 
   getCategories() {
-    return this.http.get<Category[]>('http://localhost:3000/categories');
+    return this.http.get<Category[]>(`${this.baseUrl}/categories`);
   }
 }
