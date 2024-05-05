93e45aabb167791ce0a1ddb556b793c0403bd22e
Fabian Rivera <frakxman@hotmail.com>
2024-05-05 02:12:49 -0500

    Add token interceptor to HttpClient provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+
+import { tokenInterceptor } from '@auth/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([tokenInterceptor]))
   ]
 };
